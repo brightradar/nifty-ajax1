10f9ca88376e0b65233b97a91eb17fc86815b347
Justin Schier <brightradar@gmail.com>
2011-01-27 09:59:10 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiftyAjax1::Application.config.secret_token = '891e504aae7814e10fb19b7aa1c173e230f287091162814f49b308377079d8b2104fdb3acfde0b075a0fd382d0c5116911dcf0a6637a09f3e8181a80494dd105'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NiftyAjax1::Application.config.session_store :cookie_store, :key => '_nifty-ajax1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NiftyAjax1::Application.config.session_store :active_record_store
